312c3e8184212f8573de811f94bc5ec2be22ec59
Anthony Candaele <info@anthonycandaele.be>
2026-05-22 08:55:58 +0200

    add employee model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Employee(models.Model):
+    emp_id = models.CharField(max_length=10, unique=True)
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    designation = models.CharField(max_length=100)
+    department = models.CharField(max_length=100)
+    salary = models.DecimalField(max_digits=10, decimal_places=2)
+    data_joined = models.DateField()
+    is_active = models.BooleanField(default=True)
